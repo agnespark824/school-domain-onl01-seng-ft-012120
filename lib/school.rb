c9888354935c4c671ab81e8e810c97be37129a67
Agnes Park <agnespark824@gmail.com>
2020-01-30 23:53:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+#  attr_accessor :grade
+#  attr_writer 
+  attr_reader :name
+ 
+  def initialize(name)
+    name=(name)
+  end
+  
+  def roster=(hash)
+  #return hash with keys of grade levels
+    @roster = hash
+
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    if hash.key?(grade)
+      hash[grade] << name
+    else 
+      hash[grade] = []
+      hash[grade] << name
+    end
+  end  
+
+  
+end
